fb8d9d97fbe9c9d2e10c3277018687c297630817
Egendor <erik_jansson123@hotmail.com>
2019-12-23 22:35:34 +0100

    Day 6 cleanup

--- Erik/day06.py
+++ Erik/day06.py
@@ -33,82 +33,7 @@ orbitCount = findOrbitsRec(planetDictionary,"COM",0)
 print(f"{orbitCount} total orbits!")
 
 
-
-""" Part 2: Make a function that finds the common root of A and B
-Modify function 1 to find the steps to certain endpoints
-Add the number of steps
-Profit"""
-
-
-
-
-
-def findKeyword(plnDict, startPoint, keyword):
-    #print("Searching",startPoint)
-    if(startPoint not in plnDict):
-        return False
-    for planet in plnDict[startPoint]:
-        if(planet == keyword):
-            return True
-        if(findKeyword(plnDict,planet,keyword)):
-            return True
-    return False
-
-def findCommonRoot(plnDict, startPoint, currDepth, firstElement, secondElement):
-    depth = currDepth + 1
-    if(startPoint not in plnDict):
-        return False
-    for planet in plnDict[startPoint]:
-        if(findKeyword(plnDict,planet,firstElement) and findKeyword(plnDict,planet,secondElement)):
-            findCommonRoot(plnDict, planet, depth, firstElement, secondElement)
-            return True, planet
-    return False
-
-"""
-def cheatRoot(plnDict, startPoint, firstElement, secondElement):
-    lowPlan = startPoint
-    if(findCommonRoot(plnDict, lowPlan, firstElement, secondElement)):
-        lowPlan = findCommonRoot(plnDict, lowPlan, firstElement, secondElement)[1]
-        cheatRoot(plnDict, lowPlan, firstElement, secondElement)
-    return lowPlan
-"""
-
-#def findOrbitsRec(plnDict, startPoint, depth):
-#    """Takes a dict of planets and planets in orbit, the name of the "root" planet and the current number of orbits.
-#    Returns total number of orbits"""
-#    thisDepth = depth + 1
-#    totalOrbits = 0
-#    if(startPoint not in plnDict):
-#        return thisDepth -1
-#    for planet in plnDict[startPoint]:
-#        #print(thisDepth, planet)
-#        totalOrbits += findOrbitsRec(plnDict,planet,thisDepth)
-#    return totalOrbits + depth
-
-
-#print(findKeyword(planetDictionary,"COM","SAN"))
-#print(findCommonRoot(planetDictionary,"COM", 0, "YOU","SAN"))
-#print(cheatRoot(planetDictionary,"COM","YOU","SAN"))
-
-def findCommonRoot(plnDict, startPoint, depth, firstElement, secondElement):
-    """Takes a dict of planets and planets in orbit, the name of the "root" planet and the current number of orbits.
-    Returns total number of orbits"""
-    thisDepth = depth + 1
-    totalOrbits = 0
-    if((firstElement) not in fin):
-        return thisDepth -1
-    for planet in plnDict[startPoint]:
-        #print(thisDepth, planet)
-        totalOrbits += findOrbitsRec(plnDict,planet,thisDepth)
-    return totalOrbits + depth
-
-
-
-
-
-
-
-
+# Part 2
 def makeChildList(planetList):
     """Takes a list in the form of [[planet][planet in orbit],...] and returns a dict {planet:[planets in orbit]}"""
     plnDict = dict()
@@ -128,13 +53,16 @@ def makeParentList(planetList):
             plnDict[pair[1]] = pair[0]
     return plnDict
 
-
-                
-childList = makeChildList(inputList)
-parentList = makeParentList(inputList)
-#print(childList)
-#print(parentList)
-
+def findKeyword(plnDict, startPoint, keyword):
+    #print("Searching",startPoint)
+    if(startPoint not in plnDict):
+        return False
+    for planet in plnDict[startPoint]:
+        if(planet == keyword):
+            return True
+        if(findKeyword(plnDict,planet,keyword)):
+            return True
+    return False
 
 def solveSteps(childList, parentList, key1, key2, currentSteps):
     currentNode = parentList[key1]
@@ -145,10 +73,8 @@ def solveSteps(childList, parentList, key1, key2, currentSteps):
         return [currentNode, steps]
     return solveSteps(childList, parentList, currentNode, key2, steps)
 
-#print(findKeyword(childList, "D", "SAN"))
-#solveSteps(childList, parentList, "YOU", "SAN", 0)
-#print(
-#print(findKeyword(childList, "D", "SAN"))
+childList = makeChildList(inputList)
+parentList = makeParentList(inputList)
 youStep = solveSteps(childList,parentList,"YOU","SAN",0)
 sanStep = solveSteps(childList,parentList, "SAN", "YOU",0)
 
