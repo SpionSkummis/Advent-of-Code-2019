282e74c4caa84aef09a30f27d52dc70d68e0298e
Egendor <erik_jansson123@hotmail.com>
2019-12-26 17:21:29 +0100

    Day 24 part 1 and 2

--- Erik/day24.py
+++ Erik/day24.py
@@ -51,14 +51,11 @@ def calcBiodiversity(golGrid):
     for y in range(len(golGrid)):
         for x in range(len(golGrid[0])):
             if(golGrid[y][x]):
-                print(x, y, (2 ** i))
+                #print(x, y, (2 ** i))
                 bioSum += (2 ** i)
             i += 1
     return bioSum
 
-def gridConvert(golGrid):
-    """Takes a grid, outputs as string in order to add to sets"""
-    pass
 
 def findRepeat(golGrid):
     repeatFound = False
@@ -74,18 +71,92 @@ def findRepeat(golGrid):
             return generations, currBioSum
         else:
             seenBioSum.add(currBioSum)
-        
+#Solve part 1:
+firstRepeat = findRepeat(deepcopy(inputGrid))
+print(f"The biodiversity of the first repeated grid is {firstRepeat[1]} found after {firstRepeat[0]} generations.")
 
 
-def checkAdj(golGrid, xPos, yPos):
+#Part 2:
+def checkAdj2(golStack, currentLevel, xPos, yPos):
     adjBugs = 0
-    for x in range (xPos -1, xPos +2):
-        if(x > -1 and x < 5 and x != xPos and x != 2):
-            adjBugs += golGrid[yPos][x]
-    for y in range(yPos -1, yPos +2):
-        if(y > -1 and y < 5 and y != yPos y):
-            adjBugs += golGrid[y][xPos]
+    for x in [(xPos -1), (xPos + 1)]: #range (xPos -1, xPos +2):
+        if(x > -1 and x < 5): # and x != xPos): #  not(x == xPos or x == 2)): #x != xPos and x != 2): #Get "normal" adj.
+            adjBugs += golStack[currentLevel][yPos][x]
+
+    if(xPos == 0): # Get adj from bigger grid:
+        adjBugs += golStack[currentLevel+1][2][1]
+        #print("X1")
+    elif(xPos == 4):
+        adjBugs += golStack[currentLevel+1][2][3]
+        #print("X2")
+    elif(yPos == 2 and xPos == 1): #Get adj from smaller grid
+        for a in range(0,5):
+            adjBugs += golStack[currentLevel-1][a][0]
+            #print("X3")
+    elif(yPos == 2 and xPos == 3):
+        for a in range(0,5):
+            adjBugs += golStack[currentLevel-1][a][4]
+            #print("X4", golStack[currentLevel-1][a][4], a,x)
+
+    for y in [(yPos -1), (yPos +1)]: # range(yPos -1, yPos +2):
+        if(y > -1 and y < 5): # and y != yPos): # not (y == yPos or y == 2)): #y != yPos and y != 2): #Get "normal" adj.
+            adjBugs += golStack[currentLevel][y][xPos]
+        
+    if(yPos == 0): # Get adj from bigger grid:
+        adjBugs += golStack[currentLevel+1][1][2]
+    elif(yPos == 4):
+        adjBugs += golStack[currentLevel+1][3][2]
+
+    elif(xPos == 2 and yPos == 1): #Get adj from smaller grid
+        for a in range(0,5):
+            adjBugs += golStack[currentLevel-1][0][a]
+    elif(xPos == 2 and yPos == 3):
+        for a in range(0,5):
+            adjBugs += golStack[currentLevel-1][4][a]
+        
     return adjBugs
 
+def getBugsNum(golGrid):
+    return sum([sum(line) for line in golGrid])
+def getStackBugs(golStack):
+    return sum([getBugsNum(grid) for grid in golStack])
 
-print(findRepeat(inputGrid))
+def makeGameOfLifeStack(golGrid, stackHeight):
+    emptyGrid = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+    resultStack = list()
+    for a in range(0,stackHeight):
+        resultStack.append(deepcopy(emptyGrid))
+    resultStack[(stackHeight // 2)] = deepcopy(golGrid)
+    return resultStack
+
+def simulateMinute2(golStack):
+    """Takes a stack, returns another stack after a simulated minute"""
+    newStack = deepcopy(golStack)
+
+    for level in range(1,len(golStack)-1):
+        for y in range(len(golStack[0])):
+            for x in range(len(golStack[0][0])):
+                if(not (x == 2 and y == 2)):
+                    adjBugs = checkAdj2(golStack,level,x,y)
+                    if(golStack[level][y][x]):
+                        if(adjBugs == 1):
+                            newStack[level][y][x] = 1
+                        else:
+                            newStack[level][y][x] = 0
+                    else:
+                        if(adjBugs == 1 or adjBugs == 2):
+                            newStack[level][y][x] = 1
+                        else:
+                            newStack[level][y][x] = 0
+        newStack[level][2][2] = 0
+
+    return newStack
+
+#Solves part 2:
+mainStack = makeGameOfLifeStack(inputGrid, 210)
+resultStack = deepcopy(mainStack)
+for i in range(0,200):
+    resultStack = simulateMinute2(resultStack)
+
+part2ans = getStackBugs(resultStack)
+print(f"The answer to part two is : {part2ans}")
