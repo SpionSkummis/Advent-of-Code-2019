a7fafa8b94c8c22f7367d65f57b3f579a777c9a0
Egendor <erik_jansson123@hotmail.com>
2019-12-27 01:31:38 +0100

    Day 12 part 2

--- Erik/day12.py
+++ Erik/day12.py
@@ -1,13 +1,20 @@
-import re
-#with open("Erik/inputs/input12.txt", "r") as f:
+import re, numpy
+from copy import deepcopy
+with open("Erik/inputs/input12.txt", "r") as f:
 #with open("Erik/inputs/testinput121.txt", "r") as f:
-with open("Erik/inputs/testinput122.txt", "r") as f:
+#with open("Erik/inputs/testinput122.txt", "r") as f:
     moonList = []
+    xList = []
+    yList = []
+    zList = []
     for line in f:
         xPos = int(re.search("x=(-?[0-9]+)",line).group(1))
         yPos = int(re.search("y=(-?[0-9]+)",line).group(1))
         zPos = int(re.search("z=(-?[0-9]+)",line).group(1))
         moonList.append([[xPos,yPos,zPos],[0,0,0]])
+        xList.append([xPos,0])
+        yList.append([yPos,0])
+        zList.append([zPos,0])
 
 def updateSpeed(moonList):
     for moon1 in moonList:
@@ -53,33 +60,87 @@ def makeMoonString(moonList):
         moonStr += str(moon[0][0]) + str(moon[0][1]) + str(moon[0][2]) +  str(moon[1][0]) +  str(moon[1][1]) +  str(moon[1][2])
     return moonStr
 
-# For part 2
-moonList2 = moonList.copy()
-
+def makeMoonTuple(moonList):
+    moonTup = tuple()
+    for moon in moonList:
+        tempTup = (moon[0][0], moon[0][1], moon[0][2], moon[1][0], moon[1][1], moon[1][2])
+        moonTup += tempTup
+    return moonTup
 
 #Part 1:
+part1MoonList = deepcopy(moonList)
 for i in range(0,1000):
-    updateSpeed(moonList)
-    updatePosition(moonList)
-
-#for moon in moonList:
-#    print(moon, getPotentalEnergy(moon), getKineticEnergy(moon))
-print(getSystemEnergy(moonList))
+    updateSpeed(part1MoonList)
+    updatePosition(part1MoonList)
+totalSystemEnergy = getSystemEnergy(part1MoonList)
+print(f"The total system energy is {totalSystemEnergy}")
 
 #Part 2, unfinished:
-"""
-repeats = 0
-moonSet = set()
-moonSet.add(makeMoonString(moonList2))
-while(True):
-    repeats += 1
-    updateSpeed(moonList2)
-    updatePosition(moonList2)
+
+def updatePlaneSpeed(planeList):
+    for moon1 in planeList:
+        moon1Pos = moon1[0]
+        for moon2 in planeList:
+            moon2Pos = moon2[0]
+            if(moon1Pos > moon2Pos):
+                moon1[1] -= 1
+            elif(moon1Pos < moon2Pos):
+                moon1[1] += 1
+    return planeList
+
+def updatePlanePosition(planeList):
+    for moon in planeList:
+        moon[0] += moon[1]
+    return planeList
+
+def makePlaneTup(planeList):
+    resTup = tuple()
+    for planet in planeList:
+        resTup += tuple(planet)
+    return resTup
+
+
+def findPlaneRepeats(inputList):
+    planeList = inputList.copy()
+    repeats = 0
+    seenSet = set()
+    planeTup = makePlaneTup(planeList)
+    seenSet.add(planeTup)
+    while(True):
+        updatePlaneSpeed(planeList)
+        updatePlanePosition(planeList)
+        repeats += 1
+        planeTup = makePlaneTup(planeList)
+        if(planeTup in seenSet):
+            return planeTup, repeats
+        seenSet.add(planeTup)
+
+def findFirstState(inputList, keyTuple):
+    planeList = inputList.copy()
+    print(keyTuple)
+    repeats = 0
+    planeTup = makePlaneTup(planeList)
+    if(planeTup == keyTuple):
+        print(planeTup,keyTuple,repeats)
+        return repeats
+    while(True):
+        updatePlaneSpeed(planeList)
+        updatePlanePosition(planeList)
+        repeats += 1
+        planeTup = makePlaneTup(planeList)
+        if(planeTup == keyTuple):
+            return repeats
     
-    if(getSystemEnergy(moonList2) == 0):
-        print("Zero system energy")
-        tempStr = makeMoonString(moonList2)
-        if(tempStr in moonSet):
-            break
-print(repeats)
-"""
+def findAllRepeats(xPosList, yPosList, zPosList):
+    allRepeats = []
+    allRepeats.append(findPlaneRepeats(deepcopy(xPosList)))
+    allRepeats.append(findPlaneRepeats(deepcopy(yPosList)))
+    allRepeats.append(findPlaneRepeats(deepcopy(zPosList)))
+    return allRepeats
+
+allRepeats = findAllRepeats(xList, yList, zList)
+repList = [allRepeats[0][1], allRepeats[1][1], allRepeats[2][1]]
+for element in allRepeats:
+    print(element)
+part2ans = numpy.lcm.reduce(repList) #Går att skriva om utan numpy med gcd-funktionen ur math, om man är sugen på det
+print(f"The system returns to a previously visited state after {part2ans} repeats")
